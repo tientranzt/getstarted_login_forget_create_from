11fccb69a2c99be38f5379833039adfb83857975
Tran Hoang Tien <tienth@TS24CORP.COM>
2020-03-03 18:31:58 +0700

    fix

--- lib/page/create_account.dart
+++ lib/page/create_account.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:getstarted/widget/orange_button.dart';
 import 'package:getstarted/widget/sub_title_general.dart';
@@ -5,7 +6,6 @@ import 'package:getstarted/widget/text_field_general.dart';
 import 'package:getstarted/widget/title_general.dart';
 
 class CreateAccount extends StatefulWidget {
-
   @override
   _CreateAccountState createState() => _CreateAccountState();
 }
@@ -13,11 +13,12 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
-
+    var orientation = MediaQuery.of(context).orientation;
+    print(orientation);
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
-          onTap: ()=> Navigator.pushNamed(context, "login1"),
+          onTap: () => Navigator.pushNamed(context, "login1"),
           child: Icon(
             Icons.arrow_back_ios,
             color: Colors.grey,
@@ -28,54 +29,55 @@ class _CreateAccountState extends State<CreateAccount> {
       ),
       backgroundColor: Color(0xffffffff),
       body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(15),
+          SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Image.asset("assets/images/key.png"),
-                      ),
-                      TitleGeneral(
-                        title: "Create new Password",
-                      ),
-                      SubTitleGeneral(
-                        subTitle:
-                        "Please create a new passrod. Password must be alphanumeric",
-                      ),
-                    ],
-                  ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/key.png"),
+                    ),
+                    TitleGeneral(
+                      title: "Create new Password",
+                    ),
+                    SubTitleGeneral(
+                      subTitle:
+                          "Please create a new passrod. Password must be alphanumeric",
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Column(children: <Widget>[OrangeButton(title: "DONE",)],),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text("Dont' have and account"),
-                      Text(
-                        "SIGN UP",
-                        style: TextStyle(color: Color(0xffFF910E)),
-                      ),
-                      SizedBox(height: 5,)
-                    ],
-                  ),
+                Container(
+                    height: 300,
+                    alignment: Alignment.bottomCenter,
+                    child: OrangeButton(
+                      title: "DONE",
+                    )),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Dont' have and account"),
+                    Text(
+                      "SIGN UP",
+                      style: TextStyle(color: Color(0xffFF910E)),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    )
+                  ],
                 )
               ],
             ),
           ),
-          Container(
-          alignment: Alignment.center,
-            child: Container(
-              alignment: Alignment.center,
-              child: SingleChildScrollView(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                margin: orientation == Orientation.landscape
+                    ? EdgeInsets.only(top: 0)
+                    : EdgeInsets.only(top: 40),
                 child: Column(
                   children: <Widget>[
                     TextFieldGeneral(
@@ -87,7 +89,7 @@ class _CreateAccountState extends State<CreateAccount> {
                   ],
                 ),
               ),
-            ),
+            ],
           )
         ],
       ),
